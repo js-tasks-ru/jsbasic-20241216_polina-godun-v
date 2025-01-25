c4929fcae84e48a32127c02f99ca31059c1fbdc4
Polina Godun <polina.godun.v@gmail.com>
2025-01-25 15:04:26 +0300

    Функция калькулятора

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,29 @@
 let calculator = {
-  // ваш код
+/*  Верно, что мое решение не проходило проверку из-за prompt или тут что-то ещё?
+  read() {
+    this.a = +prompt ("Первое число", 0);
+    this.b = +prompt ("Второе число", 0);
+    if (isNaN(this.a) || isNaN(this.b)){
+      return ("Нужно число");
+    }
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
 };
-
-// НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
-window.calculator = calculator; // делает ваш калькулятор доступным глобально
+*/
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
+};
+window.calculator = calculator; 
