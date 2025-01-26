1444cb879390200e21fb8d3ffda8641dc156a620
Polina Godun <polina.godun.v@gmail.com>
2025-01-26 18:02:57 +0300

    camelCase

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,16 @@
 function camelize(str) {
-  // ваш код...
-}
+  if (!str) {
+    return str;
+  }
+  return str
+    .split('-') 
+    .map((part, index) => {
+      if (index === 0) { 
+        return part;
+      }
+      let firstLetter = part.slice(0, 1);
+      let rest = part.slice(1);
+      return `${firstLetter.toUpperCase()}${rest}`;
+    })
+    .join('');
+  }
